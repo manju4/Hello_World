df3593d1ce4ffdc905583ae40c8f81614130ed43
manju4 <manjunath444m@gmail.com>
2016-10-18 11:27:31 +0530

    Add files via upload

---
+++ Mvn.java
@@ -0,0 +1,11 @@
+package Maven;
+import Maven.core ;
+public class Mvn {
+	public static void main(String[] args){
+	core s1=new core();
+	
+	s1.setId(52);
+	s1.setName("john");
+	System.out.println(s1.id+"  "+ s1.name);
+	}
+}
